76c1bdfb0b14918d0b88e8f22c8b6bbdafe7524d
yangLe0 <yanglemail@163.com>
2025-12-01 10:05:52 +0800

    add fail()

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <limits> // 用于numeric_limits
+#include <algorithm>
 using namespace std;
 void printVector(const vector<int>& vec){
     for (const int& elem : vec) {
@@ -19,8 +21,7 @@ int sumVector(const vector<int>& vec){
     return accumulate(vec.begin(), vec.end(), 0);;
 }
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "please enter the length \n";
+    cout << "please enter the length: ";
     int length = 0;
     cin >> length;
     
@@ -29,12 +30,27 @@ int main(int argc, const char * argv[]) {
     
     cout << "then enter "<<length<<" nums" << endl;
     for(int i=0;i<length;i++){
-        int a;
-        cin >> a;
-        vec.push_back(a);
+        int num;
+        cin >> num;
+        if(cin.fail()){
+            cout << "please enter a num";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin >> num;
+        }
+        vec.push_back(num);
     }
     cout << "your enter: " ;
     printVector(vec);
-    cout << "Sum is " << sumVector(vec) << endl;
+    int sum = sumVector(vec);
+    double average = static_cast<double>(sum) / length;
+    cout << "Sum is " << sum << endl;
+    if (!vec.empty()) {
+        cout << "max is " << *max_element(vec.begin(), vec.end()) << endl;
+        cout << "min is " << *min_element(vec.begin(), vec.end()) << endl;
+        sort(vec.begin(), vec.end());
+    }
+    printVector(vec);
+    cout << "average is " << average << endl;
     return 0;
 }
