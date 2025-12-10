1e5fb62d01ae7c2eb702a158a583f0ef0899b4cf
yangLe0 <yanglemail@163.com>
2025-12-10 11:51:08 +0800

    ptr & ref

--- thread/thread_para.cpp
+++ thread/thread_para.cpp
@@ -23,9 +23,45 @@ void ThreadMain(int p1, float p2, string str, Para p4)
     this_thread::sleep_for(100ms);
     cout << "ThreadMain " <<p1<<" "<<p2<<" "<<str<<" "<<p4.name<<endl;
 }
-
+void ThreadMainPtr(Para* p)
+{
+    this_thread::sleep_for(100ms);
+    cout<<"ThreadMainPtr name = " << p->name << endl;
+}
+void ThreadMainRef(Para& p)
+{
+    this_thread::sleep_for(100ms);
+    cout<<"ThreadMainRef name = " << p.name << endl;
+}
 int main(int argc, char* argv[])
 {
+    {
+        //传递引用
+        Para p;
+        p.name = "test ref";
+        thread th(ThreadMainRef,ref(p));
+        th.join();
+    }
+    getchar();
+    
+    {
+        //传递线程指针
+        Para p;//1、传堆当中的参数2、做静态的3、参数放到一个类成员当中的，确保这个对象的生命周期和线程的生命周期是一致的。或者加锁进去
+        p.name = "test ThreadMainPtr name1";
+        thread th(ThreadMainPtr, &p);//错误，线程访问的p空间会提前释放
+        th.detach();
+    }
+    //Para 释放
+    getchar();
+    {
+        //传递线程指针
+        Para p;
+        p.name = "test ThreadMainPtr name2";
+        thread th(ThreadMainPtr, &p);
+        th.join();
+        getchar();
+    }
+    
     thread th;
     {
         float f1 = 12.1f;
